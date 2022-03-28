8a53d17ca06d02f99e996a08abfebd9603648598
bibhukjha <msgbox.ubibhu@gmail.com>
2022-03-28 14:45:26 +0530

    first

--- Calculator.py
+++ Calculator.py
@@ -7,13 +7,13 @@ def mul(x,y):
 def div(x,y):
     return x/y;
 if __name__ =="__main__":
-    num1=float(input("ENTER NUM1: "))
-    num2 = float(input("ENTER NUM2: "))
+    num1=float(input("Enter num1: "))
+    num2 = float(input("Enter num2: "))
     result1= add(num1,num2)
-    print(result1)
+    print("Addition is: ",result1)
     result2= sub(num1,num2)
-    print(result2)
+    print("Substraction is: ",result2)
     result3= mul(num1,num2)
-    print(result3)
+    print("Multiple is: ",result3)
     result4= div(num1,num2)
-    print(result4)
+    print("Division is: ",result4)
